946ff0575b8271347651bc491fcfbbbed00a99de
osama256 <51829206+osama256@users.noreply.github.com>
2020-11-12 20:28:35 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,57 @@
+import { View, StyleSheet, TouchableWithoutFeedback } from 'react-native';
+import Animated, {withRepeat,  useSharedValue,interpolate, useAnimatedStyle, useDerivedValue, withTiming } from 'react-native-reanimated'
+import React from 'react';
+
+const App: () => React$Node = () => {
+
+  const animation = useSharedValue(0)
+
+  const rotation = useDerivedValue(() => {
+
+    return interpolate(animation.value,
+      [0,360],
+      [0,360])
+  })
+
+  const animationStyle = useAnimatedStyle(() => {
+    return{
+
+      transform:[
+        {
+          rotate: rotation.value + 'deg'
+        }
+      ]
+    }
+  })
+  const startAnimation = () => {
+    animation.value = withRepeat(withTiming(90,{
+      duration:1200
+    }),
+    -1,
+
+    )
+  }
+  
+  return (
+    <View style={styles.container}>
+      <TouchableWithoutFeedback onPress={startAnimation}>
+        <Animated.View style={[styles.box,animationStyle]} />
+      </TouchableWithoutFeedback>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  box: {
+    width: 200,
+    height: 200,
+    backgroundColor: '#631d94'
+  }
+});
+
+export default App;
